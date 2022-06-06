3d7a21eb9ff3acdfe09f052aa3ffa22bf2e40661
Lucas Santos <lucassm02@gmail.com>
2022-06-05 21:54:12 -0300

    feat: add data record

--- fundamental-concepts-and-life-cycle/project/src/index.js
+++ fundamental-concepts-and-life-cycle/project/src/index.js
@@ -2,6 +2,9 @@
 import DataHandler from './data-handler.js';
 import TableMaker from './table-maker.js';
 import Terminal from './terminal.js';
+import questions from '../form.json' assert { type: 'json' };
+
+const STOP_COMMAND = ':q';
 
 const tableOptions = {
   columns: [
@@ -19,5 +22,49 @@ const dataHandler = new DataHandler();
 
 const initialData = dataHandler.get();
 tableMaker.make(initialData);
-const table = tableMaker.get();
-terminal.print(table);
+
+dataHandler.onChange((data) => {
+  tableMaker.update(data);
+});
+
+async function questionLoop(responses = {}, index = 0) {
+  const current = questions[index];
+
+  if (!current) return responses;
+
+  terminal.clear();
+  const table = tableMaker.get();
+  terminal.print(table);
+
+  terminal.print('\nDigite :q para sair;');
+  const response = await terminal.question(current.question);
+
+  if (response === STOP_COMMAND) return;
+
+  return await questionLoop(
+    { ...responses, [current.field]: response },
+    ++index
+  );
+}
+
+async function mainLoop() {
+  try {
+    const responses = await questionLoop();
+
+    if (!responses) {
+      terminal.close();
+      console.log('Finalizando...');
+      return;
+    }
+
+    const vehicles = responses.vehicles.split(',');
+
+    dataHandler.add({ ...responses, vehicles });
+
+    return mainLoop();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+await mainLoop();
