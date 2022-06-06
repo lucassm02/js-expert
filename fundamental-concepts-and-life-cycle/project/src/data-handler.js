fcb20ad9e56ecb78d6a4ed6d3719f357a6f5bc24
Lucas Santos <lucassm02@gmail.com>
2022-06-05 21:46:54 -0300

    chore: add class to manipulate the data

---
+++ fundamental-concepts-and-life-cycle/project/src/data-handler.js
@@ -0,0 +1,27 @@
+import database from '../database.json' assert { type: 'json' };
+import Person from './person.js';
+import { formateSnakeCaseKeysForCamelCase } from '@badass-team-code/formatted-cases-words';
+
+export default class DataHandler {
+  #data = [];
+  #subscribers = [];
+
+  constructor() {
+    this.#data = formateSnakeCaseKeysForCamelCase(database);
+  }
+  get(language = 'pt-BR') {
+    return this.#data.map((item) => new Person(item).formatted(language));
+  }
+  add(data) {
+    const { id: lastId } = this.#data.at(-1);
+    const newId = lastId + 1;
+    this.#data = [...this.#data, { ...data, id: newId }];
+    this.#subscribers.map((subscriber) => {
+      subscriber(this.get());
+    });
+  }
+
+  onChange(subscriber) {
+    this.#subscribers = [...this.#subscribers, subscriber];
+  }
+}
